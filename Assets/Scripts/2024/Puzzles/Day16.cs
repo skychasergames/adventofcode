400d671866810914b6209da30280d8ef93b52aca
Lyndon Forsythe <lyndon@skychasergames.com>
2024-12-23 12:20:37 +1300

    Implement AoC2024 Day 16, Puzzle 2

--- Assets/Scripts/2024/Puzzles/Day16.cs
+++ Assets/Scripts/2024/Puzzles/Day16.cs
@@ -87,61 +87,6 @@ namespace AoC2024
 			}
 		}
 
-		protected override void ExecutePuzzle1()
-		{
-			InitializeMap();
-			
-			_executePuzzleCoroutine = EditorCoroutineUtility.StartCoroutine(ExecutePuzzle(), this);
-		}
-
-		private IEnumerator ExecutePuzzle()
-		{
-			EditorWaitForSeconds interval = new EditorWaitForSeconds(_isExample ? _intervalExample : _intervalPuzzle);
-			
-			List<PathState> possiblePaths = CalculatePossiblePaths();
-			int lowestScore = int.MaxValue;
-
-			foreach (PathState possiblePath in possiblePaths.Where(path => path.foundEndCell))
-			{
-				// Reset existing highlighted path if required
-				char[] directionChars = _directions.Select(dir => dir.character).ToArray();
-				for (int y = 0; y < _map.rows; y++)
-				{
-					for (int x = 0; x < _map.columns; x++)
-					{
-						if (directionChars.Contains(_map.GetCellValue(x, y)))
-						{
-							_map.SetCellValue(x, y, '.');
-							_map.HighlightCellView(x, y, Color.white);
-						}
-					}
-				}
-
-				// Highlight this path
-				foreach (KeyValuePair<Vector2Int, char> cellVisited in possiblePath.cellsVisited)
-				{
-					if (_map.GetCellValue(cellVisited.Key) == '.')
-					{
-						_map.SetCellValue(cellVisited.Key, cellVisited.Value);
-						_map.HighlightCellView(cellVisited.Key, _pathHighlight);
-					}
-				}
-
-				int pathScore = possiblePath.GetScore();
-				LogResult("Score for path", pathScore);
-				if (pathScore < lowestScore)
-				{
-					lowestScore = pathScore;
-				}
-
-				EditorApplication.QueuePlayerLoopUpdate();
-				yield return interval;
-			}
-			LogResult("Lowest score", lowestScore);
-
-			_executePuzzleCoroutine = null;
-		}
-
 		private struct PathOption
 		{
 			public int directionIndex;
@@ -156,100 +101,18 @@ namespace AoC2024
 			}
 		}
 
-		private List<PathState> CalculatePossiblePaths()
+		protected override void ExecutePuzzle1()
 		{
-			List<PathState> completedPathStates = new List<PathState>();
-
-			Queue<(PathState pathState, PathOption option)> pendingPathStates = new Queue<(PathState pathState, PathOption option)>();
-			Dictionary<Vector2Int, int> lowestScorePerPosition = new Dictionary<Vector2Int, int>();
-
-			foreach (PathOption pathOption in GetPathOptions(_startingPathState))
-			{
-				PathState newPathState = new PathState(_startingPathState);
-				pendingPathStates.Enqueue((newPathState, pathOption));
-			}
-
-			while (pendingPathStates.Count > 0)
-			{
-				(PathState currentPathState, PathOption currentPathOption) = pendingPathStates.Dequeue();
-				PerformOption(currentPathState, currentPathOption);
-
-				// Check current path score against the previous lowest score
-				int currentPathScore = currentPathState.GetScore();
-				if (lowestScorePerPosition.TryGetValue(currentPathState.reindeerPosition, out int currentPositionLowestScore))
-				{
-					if (currentPathScore < currentPositionLowestScore)
-					{
-						// More efficient than the previous score
-						lowestScorePerPosition[currentPathState.reindeerPosition] = currentPathScore;
-					}
-					else
-					{
-						// Not the most efficient way to reach this position, abort now
-						continue;
-					}
-				}
-				else
-				{
-					// First time reaching this position
-					lowestScorePerPosition.Add(currentPathState.reindeerPosition, currentPathScore);
-				}
-
-				if (currentPathState.foundEndCell)
-				{
-					completedPathStates.Add(currentPathState);
-				}
-				else
-				{
-					foreach (PathOption option in GetPathOptions(currentPathState))
-					{
-						PathState newPathState = new PathState(currentPathState);
-						pendingPathStates.Enqueue((newPathState, option));
-					}
-				}
-			}
-
-			return completedPathStates;
-		}
-
-		private List<PathOption> GetPathOptions(PathState pathState)
-		{
-			List<PathOption> options = new List<PathOption>();
-			foreach (int adjacentDirectionIndex in GetAdjacentDirectionIndexes(pathState.reindeerDirectionIndex))
-			{
-				Direction adjacentDirection = _directions[adjacentDirectionIndex];
-				Vector2Int adjacentPosition = pathState.reindeerPosition + adjacentDirection.vector;
-				char adjacentCell = _map.GetCellValue(adjacentPosition);
-				if (adjacentCell != '#' && pathState.cellsVisited.All(cell => cell.Key != adjacentPosition))
-				{
-					options.Add(new PathOption(adjacentDirectionIndex, adjacentPosition, adjacentCell));
-				}
-			}
-
-			return options;
-		}
+			InitializeMap();
 			
-		private void PerformOption(PathState pathState, PathOption option)
-		{
-			if (option.directionIndex != pathState.reindeerDirectionIndex)
-			{
-				pathState.turnsMade++;
-				pathState.reindeerDirectionIndex = option.directionIndex;
-				pathState.reindeerDirection = _directions[pathState.reindeerDirectionIndex];
-			}
-
-			pathState.reindeerPosition += pathState.reindeerDirection.vector;
-			pathState.cellsVisited.Add(pathState.reindeerPosition, pathState.reindeerDirection.character);
-
-			if (option.cell == 'E')
-			{
-				pathState.foundEndCell = true;
-			}
+			_executePuzzleCoroutine = EditorCoroutineUtility.StartCoroutine(ExecutePuzzle(), this);
 		}
 
 		protected override void ExecutePuzzle2()
 		{
+			InitializeMap();
 			
+			_executePuzzleCoroutine = EditorCoroutineUtility.StartCoroutine(ExecutePuzzle(), this);
 		}
 
 		private void InitializeMap()
@@ -305,6 +168,167 @@ namespace AoC2024
 			_startPosition = Vector2Int.zero;
 			_endPosition = Vector2Int.zero;
 			_startingPathState = null;
+		}
+
+		private IEnumerator ExecutePuzzle()
+		{
+			EditorWaitForSeconds interval = new EditorWaitForSeconds(_isExample ? _intervalExample : _intervalPuzzle);
+			
+			List<PathState> possiblePaths = CalculatePossiblePaths();
+			List<PathState> lowestScorePaths = new List<PathState>();
+			int lowestScore = int.MaxValue;
+
+			foreach (PathState possiblePath in possiblePaths.Where(path => path.foundEndCell))
+			{
+				// Reset existing highlighted path if required
+				char[] directionChars = _directions.Select(dir => dir.character).ToArray();
+				for (int y = 0; y < _map.rows; y++)
+				{
+					for (int x = 0; x < _map.columns; x++)
+					{
+						if (directionChars.Contains(_map.GetCellValue(x, y)))
+						{
+							_map.SetCellValue(x, y, '.');
+							_map.HighlightCellView(x, y, Color.white);
+						}
+					}
+				}
+
+				// Highlight this path
+				foreach (KeyValuePair<Vector2Int, char> cellVisited in possiblePath.cellsVisited)
+				{
+					if (_map.GetCellValue(cellVisited.Key) == '.')
+					{
+						_map.SetCellValue(cellVisited.Key, cellVisited.Value);
+						_map.HighlightCellView(cellVisited.Key, _pathHighlight);
+					}
+				}
+
+				int pathScore = possiblePath.GetScore();
+				LogResult("Score for path", pathScore);
+				if (pathScore < lowestScore)
+				{
+					lowestScore = pathScore;
+					lowestScorePaths.Clear();
+					lowestScorePaths.Add(possiblePath);
+				}
+				else if (pathScore == lowestScore)
+				{
+					lowestScorePaths.Add(possiblePath);
+				}
+
+				EditorApplication.QueuePlayerLoopUpdate();
+				yield return interval;
+			}
+			LogResult("Lowest score", lowestScore);
+
+			HashSet<Vector2Int> positionsAlongLowestScorePaths = new HashSet<Vector2Int> { _startPosition };
+			foreach (Vector2Int position in lowestScorePaths.SelectMany(path => path.cellsVisited.Keys))
+			{
+				positionsAlongLowestScorePaths.Add(position);
+
+				if (_map.GetCellValue(position) != 'E')
+				{
+					_map.HighlightCellView(position, _pathHighlight);
+				}
+			}
+
+			LogResult("Total positions along lowest score paths", positionsAlongLowestScorePaths.Count);
+			EditorApplication.QueuePlayerLoopUpdate();
+
+			_executePuzzleCoroutine = null;
+		}
+
+		private List<PathState> CalculatePossiblePaths()
+		{
+			List<PathState> completedPathStates = new List<PathState>();
+
+			Queue<(PathState pathState, PathOption option)> pendingPathStates = new Queue<(PathState pathState, PathOption option)>();
+			Dictionary<(Vector2Int position, int directionIndex), int> lowestScorePerPosition = new Dictionary<(Vector2Int position, int directionIndex),int>();
+
+			foreach (PathOption pathOption in GetPathOptions(_startingPathState))
+			{
+				PathState newPathState = new PathState(_startingPathState);
+				pendingPathStates.Enqueue((newPathState, pathOption));
+			}
+
+			while (pendingPathStates.Count > 0)
+			{
+				(PathState currentPathState, PathOption currentPathOption) = pendingPathStates.Dequeue();
+				PerformOption(currentPathState, currentPathOption);
+
+				// Check current path score against the previous lowest score
+				int currentPathScore = currentPathState.GetScore();
+				(Vector2Int position, int directionIndex) positionAndDirection = (currentPathState.reindeerPosition, currentPathState.reindeerDirectionIndex);
+				if (lowestScorePerPosition.TryGetValue(positionAndDirection, out int currentPositionLowestScore))
+				{
+					if (currentPathScore <= currentPositionLowestScore)
+					{
+						// More efficient than, or equally efficient to, the previous score
+						lowestScorePerPosition[positionAndDirection] = currentPathScore;
+					}
+					else
+					{
+						// Not the most efficient way to reach this position, abort now
+						continue;
+					}
+				}
+				else
+				{
+					// First time reaching this position
+					lowestScorePerPosition.Add(positionAndDirection, currentPathScore);
+				}
+
+				if (currentPathState.foundEndCell)
+				{
+					completedPathStates.Add(currentPathState);
+				}
+				else
+				{
+					foreach (PathOption option in GetPathOptions(currentPathState))
+					{
+						PathState newPathState = new PathState(currentPathState);
+						pendingPathStates.Enqueue((newPathState, option));
+					}
+				}
+			}
+
+			return completedPathStates;
+		}
+
+		private List<PathOption> GetPathOptions(PathState pathState)
+		{
+			List<PathOption> options = new List<PathOption>();
+			foreach (int adjacentDirectionIndex in GetAdjacentDirectionIndexes(pathState.reindeerDirectionIndex))
+			{
+				Direction adjacentDirection = _directions[adjacentDirectionIndex];
+				Vector2Int adjacentPosition = pathState.reindeerPosition + adjacentDirection.vector;
+				char adjacentCell = _map.GetCellValue(adjacentPosition);
+				if (adjacentCell != '#' && pathState.cellsVisited.All(cell => cell.Key != adjacentPosition))
+				{
+					options.Add(new PathOption(adjacentDirectionIndex, adjacentPosition, adjacentCell));
+				}
+			}
+
+			return options;
+		}
+			
+		private void PerformOption(PathState pathState, PathOption option)
+		{
+			if (option.directionIndex != pathState.reindeerDirectionIndex)
+			{
+				pathState.turnsMade++;
+				pathState.reindeerDirectionIndex = option.directionIndex;
+				pathState.reindeerDirection = _directions[pathState.reindeerDirectionIndex];
+			}
+
+			pathState.reindeerPosition += pathState.reindeerDirection.vector;
+			pathState.cellsVisited.Add(pathState.reindeerPosition, pathState.reindeerDirection.character);
+
+			if (option.cell == 'E')
+			{
+				pathState.foundEndCell = true;
+			}
 		}
 	}
 }
