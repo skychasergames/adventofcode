d240c2ae33de946a66a0ea574ffe28b486f012fc
Lyndon Forsythe <lyndon@skychasergames.com>
2024-12-31 14:52:28 +1300

    Implement AoC2024 Day 18, Puzzle 2

--- Assets/Scripts/2024/Puzzles/Day18.cs
+++ Assets/Scripts/2024/Puzzles/Day18.cs
@@ -18,29 +18,28 @@ namespace AoC2024
 		[SerializeField] private DijkstraGrid _map = null;
 		[SerializeField] private Color _pathHighlight = Color.yellow;
 		[SerializeField] private Color _corruptedHighlight = Color.black;
+		[SerializeField] private Color _newCorruptedHighlight = Color.red;
+		[SerializeField] private int _cyclesPerUpdateLoop = 100;
+		[SerializeField] private float _byteInterval = 0.5f;
+
+		private List<Vector2Int> _fallingBytes = null;
 		
 		private EditorCoroutine _executePuzzleCoroutine = null;
-		
+		private bool _hasPathAvailable = true;
+		private List<Vector2Int> _currentShortestPath = new List<Vector2Int>();
+
 		protected override void ExecutePuzzle1()
 		{
 			InitializeMap();
 			
-			// Simulate initial falling bytes
-			List<Vector2Int> fallingBytes = GetFallingBytes();
-			for (int i = 0; i < (_isExample ? _exampleFallingByteCount : _puzzleFallingByteCount); i++)
-			{
-				Vector2Int coords = fallingBytes[i];
-				DijkstraGridCell cell = _map.GetCellValue(coords);
-				cell.SetBlocked(true);
-				_map.HighlightCellView(coords, _corruptedHighlight);
-			}
-			
-			_executePuzzleCoroutine = EditorCoroutineUtility.StartCoroutine(ExecutePuzzle(), this);
+			_executePuzzleCoroutine = EditorCoroutineUtility.StartCoroutine(FindShortestPath(), this);
 		}
 
 		protected override void ExecutePuzzle2()
 		{
+			InitializeMap();
 			
+			_executePuzzleCoroutine = EditorCoroutineUtility.StartCoroutine(ExecutePuzzle2Coroutine(), this);
 		}
 		
 		[Button]
@@ -60,14 +59,19 @@ namespace AoC2024
 			ResetMap();
 			
 			_map.Initialize(_isExample ? _exampleMapSize : _puzzleMapSize);
+			
+			// Simulate initial falling bytes
+			_fallingBytes = _inputDataLines.Select(line => ParseVector2Int(line, ",")).ToList();
+			for (int i = 0; i < (_isExample ? _exampleFallingByteCount : _puzzleFallingByteCount); i++)
+			{
+				Vector2Int coords = _fallingBytes[i];
+				DijkstraGridCell cell = _map.GetCellValue(coords);
+				cell.SetBlocked(true);
+				_map.HighlightCellView(coords, _corruptedHighlight);
+			}
 		}
-
-		private List<Vector2Int> GetFallingBytes()
-		{
-			return _inputDataLines.Select(line => ParseVector2Int(line, ",")).ToList();
-		}
-
-		private IEnumerator ExecutePuzzle()
+		
+		private IEnumerator FindShortestPath()
 		{
 			List<DijkstraGridCell> unvisitedCells = new List<DijkstraGridCell>(_map.cells.Cast<DijkstraGridCell>());
 			List<DijkstraGridCell> tentativeCells = new List<DijkstraGridCell>();
@@ -79,6 +83,8 @@ namespace AoC2024
 
 			Vector2Int endCoord = new Vector2Int(_map.columns-1, _map.rows-1);
 			DijkstraGridCell endCell = _map.GetCellValue(endCoord);
+
+			int cyclesSinceLastUpdate = 0;
 			
 			while (true)
 			{
@@ -106,33 +112,100 @@ namespace AoC2024
 				{
 					break;
 				}
+
+				if (tentativeCells.Count == 0)
+				{
+					Log("No connection through to end cell");
+					_hasPathAvailable = false;
+					yield break;
+				}
 				
 				currentCell = tentativeCells.OrderBy(cell => cell.BestTotalDistanceToReachCell).First();
 				
-				EditorApplication.QueuePlayerLoopUpdate();
-				yield return null;
+				// Prevent Unity from locking up during long executions
+				cyclesSinceLastUpdate++;
+				if (cyclesSinceLastUpdate >= _cyclesPerUpdateLoop)
+				{
+					cyclesSinceLastUpdate = 0;
+					
+					EditorApplication.QueuePlayerLoopUpdate();
+					yield return null;
+				}
 			}
 			
 			// Assemble and highlight the path we created
-			List<Vector2Int> bestPath = new List<Vector2Int>();
+			_currentShortestPath = new List<Vector2Int>();
 			DijkstraGridCell pathCell = endCell;
 			do
 			{
-				bestPath.Add(pathCell.Coords);
+				_currentShortestPath.Add(pathCell.Coords);
 				pathCell = pathCell.PreviousCellInPath;
 			}
 			while (pathCell != startCell);
 				
-			bestPath.Add(pathCell.Coords);
+			_currentShortestPath.Add(pathCell.Coords);
 				
-			foreach (Vector2Int cell in bestPath)
+			foreach (Vector2Int cell in _currentShortestPath)
 			{
 				_map.HighlightCellView(cell.x, cell.y, _pathHighlight);
 			}
 
 			EditorApplication.QueuePlayerLoopUpdate();
 
-			LogResult("Shortest path to exit", bestPath.Count-1);
+			LogResult("Shortest path to exit", _currentShortestPath.Count-1);
+		}
+
+		private IEnumerator ExecutePuzzle2Coroutine()
+		{
+			EditorWaitForSeconds byteInterval = new EditorWaitForSeconds(_byteInterval);
+			_hasPathAvailable = true;
+
+			int currentByte = _isExample ? _exampleFallingByteCount : _puzzleFallingByteCount;
+			Vector2Int currentByteCoords = _fallingBytes[currentByte];
+			
+			while (_hasPathAvailable)
+			{
+				// Reset all empty cells' highlight states and distance scores
+				for (int y = 0; y < _map.rows; y++)
+				{
+					for (int x = 0; x < _map.columns; x++)
+					{
+						if (!_map.GetCellValue(x, y).IsBlocked)
+						{
+							_map.GetCellValue(x, y).SetBestTotalDistanceToReachCell(int.MaxValue, null);
+							_map.HighlightCellView(x, y, Color.white);
+						}
+					}
+				}
+				
+				// Check for shortest path
+				yield return FindShortestPath();
+
+				EditorApplication.QueuePlayerLoopUpdate();
+				yield return byteInterval;
+				
+				if (_hasPathAvailable)
+				{
+					// Drop bytes onto map until path is blocked
+					do
+					{
+						currentByte++;
+						currentByteCoords = _fallingBytes[currentByte];
+						DijkstraGridCell lastByteCell = _map.GetCellValue(currentByteCoords);
+						lastByteCell.SetBlocked(true);
+						_map.HighlightCellView(currentByteCoords, _newCorruptedHighlight);
+
+						EditorApplication.QueuePlayerLoopUpdate();
+						yield return byteInterval;
+					}
+					while (!_currentShortestPath.Contains(currentByteCoords));
+				}
+				else
+				{
+					Log("Path blocked at " + currentByteCoords + " after " + currentByte + " bytes");
+					break;
+				}
+			}
 		}
 	}
 }
